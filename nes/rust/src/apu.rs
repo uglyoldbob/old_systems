fb664a725319e58109d139a1cf1057b6b0d7c0e0
Thomas Epperson <thomas.epperson@gmail.com>
2023-03-22 07:08:01 -0500

    Some implementation of the apu envelope.

--- nes/rust/src/apu.rs
+++ nes/rust/src/apu.rs
@@ -6,22 +6,56 @@ use rb::RbProducer;
 /// An envelope sequencer for the apu
 #[non_exhaustive]
 #[derive(serde::Serialize, serde::Deserialize)]
-struct ApuEnvelope {}
+struct ApuEnvelope {
+    /// Initiates reload of the timers
+    startflag: bool,
+    /// The divider for feeding the decay timer
+    divider: u8,
+    /// The counter for envelope output
+    decay: u8,
+}
 
 impl ApuEnvelope {
     /// Create a new envelope
     fn new() -> Self {
-        Self {}
+        Self {
+            startflag: false,
+            divider: 0,
+            decay: 0,
+        }
+    }
+
+    /// Returns the audio level of the envelope
+    fn audio_output(&self, regs: &[u8]) -> u8 {
+        let cv_flag = (regs[0] & 0x10) != 0;
+        if cv_flag {
+            regs[0] & 0xF
+        } else {
+            self.decay
+        }
     }
 
     /// Clock the envelope
     fn clock(&mut self, regs: &[u8]) {
         let cv = regs[0] & 0xF;
-        //false means use volume from envelope, otherwise use cv for volume
-        let cv_flag = (regs[0] & 0x10) != 0;
-        // True when the envelope should loop?
+        // True when the envelope should loop
         let eloop = (regs[0] & 0x20) != 0;
 
+        if !self.startflag {
+            if self.divider == 0 {
+                self.divider = cv;
+                if self.decay > 0 {
+                    self.decay -= 1;
+                }
+                else if eloop {
+                    self.decay = 15;
+                }
+            }
+        }
+        else {
+            self.decay = 15;
+            self.divider = cv;
+        }
     }
 }
 
