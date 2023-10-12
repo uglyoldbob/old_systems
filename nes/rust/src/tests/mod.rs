cd8b9d40312b016c2adb4b5b921e0fc8b7be49b7
Thomas Epperson <thomas.epperson@gmail.com>
2023-10-12 15:50:24 -0500

    Tweak an apu test.

--- nes/rust/src/tests/mod.rs
+++ nes/rust/src/tests/mod.rs
@@ -1093,7 +1093,7 @@ fn apu_test15() {
     loop {
         nes_data.cycle_step(&mut None, &mut None);
         if nes_data.cpu_peripherals.ppu_frame_end()
-            && nes_data.cpu_peripherals.ppu_frame_number() == 30
+            && nes_data.cpu_peripherals.ppu_frame_number() == 50
         {
             break;
         }
