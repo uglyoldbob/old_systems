948b1105a0c1cd1f9853a349dd05821448abbd00
Thomas Epperson <thomas.epperson@gmail.com>
2024-12-04 11:57:07 -0600

    Fix typo.

--- nes/__init__.py
+++ nes/__init__.py
@@ -46,7 +46,7 @@ def data_file(f):
 class Nes:
     def __init__(self, platform):
         platform.add_source(os.path.join(data_location, "clocked_sram_init.vhd"))
-        platform.add_source(os.path.join(data_location, "clcoked_sram.vhd"))
+        platform.add_source(os.path.join(data_location, "clocked_sram.vhd"))
         platform.add_source(os.path.join(data_location, "delay_line.vhd"))
         platform.add_source(os.path.join(data_location, "frame_sync.vhd"))
         platform.add_source(os.path.join(data_location, "hdmi.vhd"))
