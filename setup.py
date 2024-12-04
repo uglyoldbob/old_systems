0dd2159a681a4b0873c6ad85566acc0a408d918e
Thomas Epperson <thomas.epperson@gmail.com>
2024-12-04 11:00:30 -0600

    Modify path again.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ Python module containing hdl files for emulating old consoles.""",
     zip_safe=False,
     packages=setuptools.find_packages(),
     package_data={
-    	'nes': ['nes/hdl/**'],
+    	'nes': ['hdl/**'],
     },
     include_package_data=True,
     project_urls={
